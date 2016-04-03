raroni/lockstep

commit dfd7731de42741d737880da77417326ebef90c0b

Added memory barrier to chunk ring buffer

code/lib/chunk_ring_buffer.cpp
@@ -43,9 +43,12 @@ void ChunkRingBufferWrite(crb *Buffer, const void *Data, memsize Length) {
   Buffer->Sizes[Buffer->WriteIndex] = Length;
   void *Destination = ((ui8*)Buffer->Data) + WriteOffset;
   memcpy(Destination, Data, Length);
+  Buffer->Offsets[Buffer->WriteIndex] = WriteOffset + Length;
+
+  MemoryBarrier;
+
   Buffer->WriteIndex = (Buffer->WriteIndex + 1) % Buffer->ChunkCount;
   Assert(Buffer->WriteIndex != Buffer->ReadIndex);
-  Buffer->Offsets[Buffer->WriteIndex] = WriteOffset + Length;
 }
 
 memsize ChunkRingBufferRead(crb *Buffer, void **Chunk) {

commit cde147c35df0b69f608e1e603179b7085290ae25

Chunk ring buffer bugfix
Commit dfd7731de42741d737880da77417326ebef90c0b introduced a bug in the chunk ring buffer. Luckily, the tests caught it.

code/lib/chunk_ring_buffer.cpp
@@ -26,6 +26,9 @@ void InitChunkRingBuffer(
 }
 
 void ChunkRingBufferWrite(crb *Buffer, const void *Data, memsize Length) {
+  memsize NewWriteIndex = (Buffer->WriteIndex + 1) % Buffer->ChunkCount;
+  Assert(NewWriteIndex != Buffer->ReadIndex);
+
   memsize ReadOffset = Buffer->Offsets[Buffer->ReadIndex];
   memsize WriteOffset = Buffer->Offsets[Buffer->WriteIndex];
   if(ReadOffset <= WriteOffset) {
@@ -43,12 +46,11 @@ void ChunkRingBufferWrite(crb *Buffer, const void *Data, memsize Length) {
   Buffer->Sizes[Buffer->WriteIndex] = Length;
   void *Destination = ((ui8*)Buffer->Data) + WriteOffset;
   memcpy(Destination, Data, Length);
-  Buffer->Offsets[Buffer->WriteIndex] = WriteOffset + Length;
+  Buffer->Offsets[NewWriteIndex] = WriteOffset + Length;
 
   MemoryBarrier;
 
-  Buffer->WriteIndex = (Buffer->WriteIndex + 1) % Buffer->ChunkCount;
-  Assert(Buffer->WriteIndex != Buffer->ReadIndex);
+  Buffer->WriteIndex = NewWriteIndex;
 }
 
 memsize ChunkRingBufferRead(crb *Buffer, void **Chunk) {