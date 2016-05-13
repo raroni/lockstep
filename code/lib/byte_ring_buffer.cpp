eb2148cc1a2e0d1429d63b3896a737ccc9da6c33
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2016-05-13 09:52:10 +0200

    Byte ring buffer now using new C11 atomics

--- code/lib/byte_ring_buffer.cpp
+++ code/lib/byte_ring_buffer.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdatomic.h>
 #include "math.h"
 #include "assert.h"
 #include "memory_barrier.h"
@@ -8,11 +9,13 @@ typedef byte_ring_buffer brb;
 
 memsize ByteRingBufferCalcUsage(brb *Buffer) {
   memsize Result;
-  if(Buffer->ReadPos <= Buffer->WritePos) {
-    Result = Buffer->WritePos - Buffer->ReadPos;
+  memsize WritePos = atomic_load_explicit(&Buffer->WritePos, memory_order_acquire);
+  memsize ReadPos = atomic_load_explicit(&Buffer->ReadPos, memory_order_acquire);
+  if(ReadPos <= WritePos) {
+    Result = WritePos - ReadPos;
   }
   else {
-    Result = Buffer->Storage.Length - Buffer->ReadPos + Buffer->WritePos;
+    Result = Buffer->Storage.Length - ReadPos + WritePos;
   }
   return Result;
 }
@@ -24,21 +27,23 @@ memsize ByteRingBufferCalcFree(brb *Buffer) {
 
 void InitByteRingBuffer(brb *Buffer, buffer Storage) {
   Buffer->Storage = Storage;
-  Buffer->WritePos = 0;
-  Buffer->ReadPos = 0;
+  Buffer->WritePos = ATOMIC_VAR_INIT(0);
+  Buffer->ReadPos = ATOMIC_VAR_INIT(0);
 }
 
 void ByteRingBufferWrite(brb *Buffer, buffer Input) {
   Assert(ByteRingBufferCalcFree(Buffer) > Input.Length);
 
-  if(Buffer->Storage.Length - Buffer->WritePos >= Input.Length) {
-    void *Destination = ((ui8*)Buffer->Storage.Addr) + Buffer->WritePos;
+  memsize WritePos = atomic_load_explicit(&Buffer->WritePos, memory_order_relaxed);
+
+  if(Buffer->Storage.Length - WritePos >= Input.Length) {
+    void *Destination = ((ui8*)Buffer->Storage.Addr) + WritePos;
     memcpy(Destination, Input.Addr, Input.Length);
   }
   else {
-    void *Destination1 = ((ui8*)Buffer->Storage.Addr) + Buffer->WritePos;
+    void *Destination1 = ((ui8*)Buffer->Storage.Addr) + WritePos;
     const void *Source1 = Input.Addr;
-    memsize WriteSize1 = Buffer->Storage.Length - Buffer->WritePos;
+    memsize WriteSize1 = Buffer->Storage.Length - WritePos;
 
     void *Destination2 = Buffer->Storage.Addr;
     const void *Source2 = ((ui8*)Input.Addr) + WriteSize1;
@@ -48,23 +53,24 @@ void ByteRingBufferWrite(brb *Buffer, buffer Input) {
     memcpy(Destination2, Source2, WriteSize2);
   }
 
-  MemoryBarrier;
-
-  Buffer->WritePos = (Buffer->WritePos + Input.Length) % Buffer->Storage.Length;
+  memsize NewWritePos = (WritePos + Input.Length) % Buffer->Storage.Length;;
+  atomic_store_explicit(&Buffer->WritePos, NewWritePos, memory_order_release);
 }
 
 memsize ByteRingBufferPeek(brb *ByteRingBuffer, buffer Output) {
   memsize Usage = ByteRingBufferCalcUsage(ByteRingBuffer);
   memsize ReadLength = MinMemsize(Output.Length, Usage);
 
-  if(ByteRingBuffer->Storage.Length - ByteRingBuffer->ReadPos >= ReadLength) {
-    void *Source = ((ui8*)ByteRingBuffer->Storage.Addr) + ByteRingBuffer->ReadPos;
+  memsize ReadPos = atomic_load_explicit(&ByteRingBuffer->ReadPos, memory_order_relaxed);
+
+  if(ByteRingBuffer->Storage.Length - ReadPos >= ReadLength) {
+    void *Source = ((ui8*)ByteRingBuffer->Storage.Addr) + ReadPos;
     memcpy(Output.Addr, Source, ReadLength);
   }
   else {
     void *Destination1 = Output.Addr;
-    const void *Source1 = ((ui8*)ByteRingBuffer->Storage.Addr + ByteRingBuffer->ReadPos);
-    memsize ReadLength1 = ByteRingBuffer->Storage.Length - ByteRingBuffer->ReadPos;
+    const void *Source1 = ((ui8*)ByteRingBuffer->Storage.Addr + ReadPos);
+    memsize ReadLength1 = ByteRingBuffer->Storage.Length - ReadPos;
 
     void *Destination2 = ((ui8*)Output.Addr) + ReadLength1;
     const void *Source2 = ByteRingBuffer->Storage.Addr;
@@ -78,7 +84,9 @@ memsize ByteRingBufferPeek(brb *ByteRingBuffer, buffer Output) {
 }
 
 void ByteRingBufferReadAdvance(brb *Ring, memsize Length) {
-  Ring->ReadPos = (Ring->ReadPos + Length) % Ring->Storage.Length;
+  memsize OldReadPos = atomic_load_explicit(&Ring->ReadPos, memory_order_relaxed);
+  memsize NewReadPos = (OldReadPos + Length) % Ring->Storage.Length;
+  atomic_store_explicit(&Ring->ReadPos, NewReadPos, memory_order_release);
 }
 
 memsize ByteRingBufferRead(brb *Ring, buffer Output) {
@@ -88,14 +96,14 @@ memsize ByteRingBufferRead(brb *Ring, buffer Output) {
   return ReadLength;
 }
 
-void ByteRingBufferReset(brb *Ring) {
-  Ring->WritePos = 0;
-  Ring->ReadPos = 0;
+void TerminateByteRingBuffer(brb *Buffer) {
+  ByteRingBufferReset(Buffer);
+  Buffer->Storage.Addr = NULL;
+  Buffer->Storage.Length = 0;
 }
 
-void TerminateByteRingBuffer(brb *Buffer) {
-  Buffer->Storage.Addr = NULL;
-  Buffer->WritePos = 0;
-  Buffer->ReadPos = 0;
-  Buffer->Storage.Length = 0;
+/* Not thread-safe */
+void ByteRingBufferReset(brb *Ring) {
+  atomic_store_explicit(&Ring->WritePos, 0, memory_order_relaxed);
+  atomic_store_explicit(&Ring->ReadPos, 0, memory_order_relaxed);
 }
--- code/lib/byte_ring_buffer.h
+++ code/lib/byte_ring_buffer.h
@@ -4,8 +4,8 @@
 
 struct byte_ring_buffer {
   buffer Storage;
-  memsize ReadPos;
-  memsize WritePos;
+  _Atomic memsize ReadPos;
+  _Atomic memsize WritePos;
 };
 
 void InitByteRingBuffer(byte_ring_buffer *Buffer, buffer Storage);
@@ -14,5 +14,7 @@ memsize ByteRingBufferRead(byte_ring_buffer *Buffer, buffer Output);
 memsize ByteRingBufferPeek(byte_ring_buffer *Buffer, buffer Output);
 void ByteRingBufferReadAdvance(byte_ring_buffer *Buffer, memsize Length);
 memsize ByteRingBufferCalcFree(byte_ring_buffer *Buffer);
-void ByteRingBufferReset(byte_ring_buffer *Buffer);
 void TerminateByteRingBuffer(byte_ring_buffer *Buffer);
+
+/* Not thead safe */
+void ByteRingBufferReset(byte_ring_buffer *Buffer);
